de9875fa3fd47170613ef92ffb29655e323a4af7
Elad Shaham <7040645+eshaham@users.noreply.github.com>
2026-01-11 12:09:07 +0200

    Fix Mantine hydration error by disabling Turbopack

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       <head>
         <ColorSchemeScript />
       </head>
-      <body suppressHydrationWarning>
+      <body>
         <MantineProvider>{children}</MantineProvider>
       </body>
     </html>
